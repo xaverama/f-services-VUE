05ee4667608b4edadc6b2aca62755375d6f2804d
xavergruber <xavergruber@webundsoehne.com>
2022-08-03 09:29:43 +0200

    blank project template for services

--- services-VUE/src/App.vue
+++ services-VUE/src/App.vue
@@ -1,25 +1,13 @@
 <template>
   <h1>Services</h1>
   <div class="container">
-   <h2>"{{quote}}"</h2>
-    <br>
-    <p>-Kanye West</p>
+
   </div>
 
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from "vue";
-import {dataService} from "./services/data-service";
 
-const data = dataService();
-
-const quote = ref("");
-
-
-onMounted(async () => {
-  quote.value = await data.getAllData()
-})
 
 </script>
 
@@ -29,9 +17,7 @@ onMounted(async () => {
   flex-direction: column;
   gap: 1rem;
 }
-p{
-  text-align: right;
-}
+
 
 
 </style>
